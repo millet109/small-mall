26aea36635d4a235fc3dbe63142a9e18ff69dc3c
guwenjie0375 <guguguwenjie@163.com>
2018-09-18 16:57:46 +0800

    修改日志记录记录系统（位置及记录级别自定义）

--- application/lib/exception/ExceptionHandler.php
+++ application/lib/exception/ExceptionHandler.php
@@ -10,6 +10,7 @@ namespace app\lib\exception;
 
 use Exception;
 use think\exception\Handle;
+use think\Log;
 use think\Request;
 
 class ExceptionHandler extends Handle
@@ -18,6 +19,11 @@ class ExceptionHandler extends Handle
     private $msg;
     private $errorCode;
 
+    /**
+     * 自定义异常处理机制
+     * @param Exception $e
+     * @return \think\Response|\think\response\Json
+     */
     public function render(Exception $e)
     {
         if($e instanceof BaseException){
@@ -26,11 +32,16 @@ class ExceptionHandler extends Handle
             $this->msg = $e->msg;
             $this->errorCode = $e->errorCode;
         }else{
+            // 如果是服务器未处理的异常，将http状态码设置为500，并记录日志
+            if(config('app_debug')){
+                // 调试状态下需要显示TP默认的异常页面，因为TP的默认页面很容易看出问题
+                return parent::render($e);
+            }
 
             $this->code = 500;
             $this->msg = 'sorry，we make a mistake. (^o^)Y';
             $this->errorCode = 999;
-
+            $this->recordErrorLog($e);
         }
 
         $request = Request::instance();
@@ -40,5 +51,19 @@ class ExceptionHandler extends Handle
             'request_url' => $request = $request->url()
         ];
         return json($result, $this->code);
+    }
+
+    /**
+     * 将异常写入日志
+     * @param Exception $e
+     */
+    private function recordErrorLog(Exception $e)
+    {
+        Log::init([
+            'type'  =>  'File',
+            'path'  =>  LOG_PATH,
+            'level' => ['error']
+        ]);
+        Log::record($e->getMessage(),'error');
     }
 }
