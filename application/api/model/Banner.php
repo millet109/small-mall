e4b37c70bc2976bb13a95a78a9be6e9774058d4e
guwenjie0375 <guguguwenjie@163.com>
2018-09-25 17:01:01 +0800

    开启SQL日志记录

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -12,9 +12,20 @@ use think\Exception;
 
 class Banner
 {
+    /**
+     * 通过banner_id获取banner内容
+     * @param $id
+     * @return false|\PDOStatement|string|\think\Collection
+     */
     public static function getBannerByID($id)
     {
-        $res = Db::query("select * from banner_item where banner_id=?",[$id]);
+        //$res = Db::query("select * from banner_item where banner_id=?",[$id]);
+        //$res = Db::table('banner_item')->where('banner_id','=',$id)->select();
+        $res = Db::table('banner_item')
+            ->where(function ($query) use ($id){
+                $query->where('banner_id','=',$id)->where(true);
+            })
+            ->select();
         return $res;
     }
 }
